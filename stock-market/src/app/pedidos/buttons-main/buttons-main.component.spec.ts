dcf8e737f562074035e378a06593ae87e81e5a57
Andrea Sarmiento <anbesamo@gmail.com>
2021-10-23 08:00:27 -0400

    AndyPrueba added buttons

---
+++ stock-market/src/app/pedidos/buttons-main/buttons-main.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonsMainComponent } from './buttons-main.component';
+
+describe('ButtonsMainComponent', () => {
+  let component: ButtonsMainComponent;
+  let fixture: ComponentFixture<ButtonsMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ButtonsMainComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ButtonsMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
